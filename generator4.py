31ec207c686cc0ee1a1cdc627c3d18c38290f6df
VPlotnikov2006 <94298922+VPlotnikov2006@users.noreply.github.com>
2023-01-21 14:30:52 +0300

    try/except generator4

--- generator4.py
+++ generator4.py
@@ -1,6 +1,15 @@
-import json  # Сохранение списков
-import os  # Переход в другую директорию
-import graphviz  # Отрисовка графа
+import json  # Сохранение словарей
+import os  # Переход в директорию бота
+import sys  # Завершение работы программы
+try:
+    import graphviz  # Отрисовка графа
+except ModuleNotFoundError:
+    print('Модуль graphviz не установлен')
+    print('Команда для установки: ')
+    print('pip install graphviz')
+    input()
+    sys.exit(1)
+
 
 def print_ireq(ireq):
     """Функция вывода требований по инвентарю"""
@@ -28,11 +37,24 @@ cls = 'cls' if os.name == 'nt' else 'clear'
 sep = '\\' if os.name == 'nt' else '/'
 file = input('Введите адрес папки для бота:\n')
 s = file + f'{sep}TelegramBotData{sep}static'
-os.chdir(s)
+try:
+    os.chdir(s)
+except FileNotFoundError:
+    print('Указанной папки не существует')
+    input()
+    sys.exit(1)
 
 # Требования по предметам в инвентаре
 # Считывание требований
-inv_req = json.load(open('inventory_req_list.json', 'r'))
+try:
+    inv_req = json.load(open('inventory_req_list.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/inventory_req_list.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('[]')
+    input()
+    sys.exit(1)
 
 # Вывод требований
 print_ireq(inv_req)
@@ -48,9 +70,14 @@ print_ireq(inv_req)
 
 # Добавление новых требований
 for _ in range(int(input('Введите количество требований по инвентарю, которые вы хотите сейчас добавить: '))):
-    u = int(input('Введите индекс вершины (индексация с нуля): '))
-    t = int(input('Введите индекс вершины инвентаря (индексация с нуля): '))
-    inv_req.append([u, t])
+    try:
+        u = int(input('Введите индекс вершины (индексация с нуля): '))
+        t = int(input('Введите номер поля инвентаря (индексация с нуля): '))
+    except ValueError:
+        print('Ошибка ввода')
+        print('Убедитесь что вы ввели одно число')
+    else:
+        inv_req.append([u, t])
 
 # Сохранение
 open('inventory_req_list.json', 'w').write(json.dumps(inv_req, indent=4, ensure_ascii=False))
@@ -58,7 +85,15 @@ os.system(cls)
 
 # Требования по посещенным вершинам
 # Считывание требований
-visit_req = json.load(open('visited_req_list.json', 'r'))
+try:
+    visit_req = json.load(open('visited_req_list.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/visited_req_list.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('[]')
+    input()
+    sys.exit(1)
 
 # Вывод требований
 print_vreq(visit_req)
@@ -73,10 +108,15 @@ for _ in range(int(input('Введите количество удалений, 
 print_vreq(visit_req)
 
 # Добавление новых требований
-for _ in range(int(input('Введите количество требований по посещённым вершинам, которые вы хотите сейчас добавить: '))):
-    u = int(input('Введите индекс вершины (индексация с нуля): '))
-    t = int(input('Введите индекс вершины, которую нужно посетить (индексация с нуля): '))
-    visit_req.append([u, t])
+for _ in range(int(input('Введите количество требований по инвентарю, которые вы хотите сейчас добавить: '))):
+    try:
+        u = int(input('Введите индекс вершины (индексация с нуля): '))
+        t = int(input('Введите индекс вершины, которую нужно посетить (индексация с нуля): '))
+    except ValueError:
+        print('Ошибка ввода')
+        print('Убедитесь что вы ввели одно число')
+    else:
+        visit_req.append([u, t])
 
 # Сохранение
 open('visited_req_list.json', 'w').write(json.dumps(visit_req, indent=4, ensure_ascii=False))
@@ -84,57 +124,110 @@ os.system(cls)
 
 # Обновления полей инвентаря
 # Считывание
-inv_list = json.load(open('inventory_list.json', 'r'))
+try:
+    inv_list = json.load(open('inventory_list.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/inventory_list.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('[[], [], [], ... столько раз, сколько вершин в графе]')
+    input()
+    sys.exit(1)
+
 
 # Вывод
 print_ilist(inv_list)
 
 # Удаление ненужных обновлений
 for _ in range(int(input('Введите количество удалений, которое хотите сделать: '))):
-    u = int(input('Введите индекс вершины для которой производится удаление: '))
-    t = int(input('Введите номер поля инвенторя (индексайия с нуля): '))
-    while t in inv_list[u]:
-        inv_list[u].remove(t)
-
+    try:
+        u = int(input('Введите индекс вершины для которой производится удаление: '))
+        t = int(input('Введите номер поля инвентаря (индексайия с нуля): '))
+    except ValueError:
+        print('Ошибка ввода')
+        print('Убедитесь что вы ввели одно число')
+    else:
+        while t in inv_list[u]:
+            inv_list[u].remove(t)
+    
 # Вывод
 print_ilist(inv_list)
 
 # Обновление
 for _ in range(int(input('Введите количество изменений инвентаря: '))):
-    u = int(input('Введите индекс вершины, в которой меняется инвентарь (индексация с нуля): '))
-    t = int(input('Введите индекс вершины инвентаря (индексация с нуля): '))
-    inv_list[u].append(t)
+    try:
+        u = int(input('Введите индекс вершины, в которой меняется инвентарь (индексация с нуля): '))
+        t = int(input('Введите индекс вершины инвентаря (индексация с нуля): '))
+    except ValueError:
+        print('Ошибка ввода')
+        print('Убедитесь что вы ввели одно число')
+    else:
+        inv_list[u].append(t)
+
 
 # Сохранение
 open('inventory_list.json', 'w').write(json.dumps(inv_list, indent=4, ensure_ascii=False))
 os.system(cls)
 
-# Указание путя для отрисовки графов
-os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin' #
+# Указание пути для отрисовки графов
+if os.name == 'nt':
+    os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin'
 
-adj_list = json.load(open('adjacency_list.json', 'r'))
-button_list = json.load(open('button.json', 'r'))
-text_list = json.load(open('text.json', 'r'))
+try:
+    adj_list = json.load(open('adjacency_list.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/adjacency_list.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('[[], [], [], ... столько раз, сколько вершин в графе]')
+    input()
+    sys.exit(1)
 
+try:
+    button_list = json.load(open('button.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/button.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('[["", "", ... столько раз, сколько вершин в графе], ["", "", ... столько раз, сколько вершин в графе], '
+          '... столько раз, сколько вершин в графе]')
+    input()
+    sys.exit(1)
+
+try:
+    text_list = json.load(open('text.json', 'r'))
+except FileNotFoundError:
+    print('В файлах бота отсутствует TelegramBotData/static/text.json')
+    print('Вы можете создать его сами')
+    print('В этот файл нужно записать такую строку: ')
+    print('["", "", ... столько раз, сколько вершин в графе]')
+    input()
+    sys.exit(1)
 # Указание, куда сохранять картинку
 s = file + f'{sep}TelegramBotData{sep}graph'
 os.chdir(s)
 
 # Создание графа
-g = graphviz.Digraph('Graph for bot', comment='Your graph', format='png')
+try:
+    g = graphviz.Digraph('Graph for bot', comment='Your graph', format='png')
 
-for i in range(len(adj_list)):
-    g.node(f'{i}', text_list[i] if text_list[i] else f'No text for vertex №{i}')
+    for i in range(len(adj_list)):
+        g.node(f'{i}', text_list[i] if text_list[i] else f'No text for vertex №{i}')
 
-for i, h in enumerate(adj_list):
-    for j in h:
-        g.edge(f'{i}', f'{j}',
-               label=button_list[i][j] if button_list[i][j] else f'No text wor edge from {i} to {j}')
+    for i, h in enumerate(adj_list):
+        for j in h:
+            g.edge(f'{i}', f'{j}',
+                   label=button_list[i][j] if button_list[i][j] else f'No text wor edge from {i} to {j}')
 
-print('Вы хотите увидеть получившийся граф?(Y/N)')
-if input().upper() == 'Y':
-    g.view()
-else:
-    g.render()
+    print('Вы хотите увидеть получившийся граф?(Y/N)')
+    if input().upper() == 'Y':
+        g.view()
+    else:
+        g.render()
 
-print(f'Граф сохранён по адресу {file}{sep}TelegramBotData{sep}graph')
+    print(f'Граф сохранён по адресу {s}')
+except Exception as e:
+    print('При создании графа бота возникли проблемы')
+    print('Убедитесь что вы скачали graphviz (не только библиотеку, но и само приложение)')
+    print(e)
+    input()
